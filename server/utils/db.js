edef6bc42d6c88f352fb7d779e54134eac6649d7
ugyentring <12220096.gcit@rub.edu.bt>
2023-11-13 16:46:03 +0600

    added the env files and usage in the project

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const MONGODB_URI =
   "mongodb+srv://tsheringugyen483:thapa123@cluster0.kqadtvy.mongodb.net/thapa";
 
+const URI = process.env.MONGO_URI;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(URI);
     console.log("connection successful");
   } catch (error) {
     console.error("Database connection failed");
