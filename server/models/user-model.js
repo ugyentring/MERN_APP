702cfbfe7fc01faf30c6c66a5cb27ca8c0a080cf
ugyentring <12220096.gcit@rub.edu.bt>
2023-11-14 17:59:02 +0600

    added user model

---
+++ server/models/user-model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
